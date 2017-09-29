7689b37a5606ec9cd503ea00b72041b1a17805cb
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-28 21:27:51 -0400

    working on the methods

--- book_lending.rb
+++ book_lending.rb
@@ -3,18 +3,38 @@ class Book
 # Your class should have two class variables: @@on_shelf and @@on_loan. Both should start as empty arrays. @@on_shelf will contain the collection of book objects that are available to be lent out and @@on_loan will contain the collection of books that are currently being borrowed.
   @@on_shelf = []
   @@on_loan = []
-  @@due_date
+  @@due_date = 0
 
   # Your class will also need reader and writer methods for due_date
   def self.write_due_date=(due_date)
     due_date = @@due_date
   end
 
+  def self.create #class method
+    @@on_shelf << Book.new(title, author, isbn) #calls Book.new and shovels entry into array
+    return @@on_shelf.last #returns the newest book added to the on_shelf array
+  end
+
   def read_due
     @@due_date
   end
 
-
+  # Your class should have the following methods:
+    # an instance method initialize
+    def initialize(title, author, isbn)
+      @title = title
+      @author = author
+      @isbn = isbn
+    end
+    # an instance method borrow
+    # an instance method return_to_library
+    # an instance method lent_out?
+    # a class method create
+    # a class method current_due_date
+    # a class method overdue_books
+    # a class method browse
+    # a class method available
+    # a class method borrowed
 
 
 
