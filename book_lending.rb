ef653db57890770008d739d2ecb2e64af5c0471b
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-28 22:48:58 -0400

    added some more classes and all works but no data yet

--- book_lending.rb
+++ book_lending.rb
@@ -10,7 +10,7 @@ class Book
     due_date = @@due_date
   end
 
-  def self.create ## a class method create
+  def self.create(title, author, isbn) ## a class method create
     @@on_shelf << Book.new(title, author, isbn) #calls Book.new and shovels entry into array
     return @@on_shelf.last #returns the newest book added to the on_shelf array
   end
@@ -21,23 +21,23 @@ class Book
   end
 
   # a class method overdue_books
-  def class.overdue_books
+  def self.overdue_books
 
   end
   # a class method browse
   def self.browse
 
   end
-  # a class method available
+  # a class method available that returns value @@on_shelf
   def self.available
-
+    @@on_shelf
   end
-  # a class method borrowed
+  # a class method borrowed that returns@@on_loan
   def self.borrowed
-
+    @@on_loan
   end
 
-  def read_due
+  def due_date
     @@due_date
   end
 
@@ -71,3 +71,29 @@ class Book
 
 
 end
+
+
+
+
+
+
+sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
+aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
+if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
+puts Book.browse.inspect # #<Book:0x00555e82acdab0 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221"> (this value may be different for you)
+puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
+puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
+puts Book.available.inspect # [#<Book:0x00555e82acde20 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431">, #<Book:0x00555e82acdce0 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00555e82acdab0 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
+puts Book.borrowed.inspect # []
+puts sister_outsider.lent_out? # false
+puts sister_outsider.borrow # true
+puts sister_outsider.lent_out? # true
+puts sister_outsider.borrow # false
+puts sister_outsider.due_date # 2017-02-25 20:52:20 -0500 (this value will be different for you)
+puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
+puts Book.borrowed.inspect # [#<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=2017-02-25 20:55:17 -0500>]
+puts Book.overdue_books.inspect # []
+puts sister_outsider.return_to_library # true
+puts sister_outsider.lent_out? # false
+puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">, #<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=nil>]
+puts Book.borrowed.inspect # []
